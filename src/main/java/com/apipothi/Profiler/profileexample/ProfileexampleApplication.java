e6123ae2fd82691460b73381dff47b5999129e0f
Pandurnag <96949058+Pandurnag@users.noreply.github.com>
2022-02-17 22:21:59 +0530

    ProfileExample

---
+++ src/main/java/com/apipothi/Profiler/profileexample/ProfileexampleApplication.java
@@ -0,0 +1,13 @@
+package com.apipothi.Profiler.profileexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfileexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfileexampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apipothi/Profiler/profileexample/ProfileexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apipothi.Profiler.profileexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfileexampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
